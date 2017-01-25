539256f41924b7fb1b8193fdb2bf9f0861ed8618
SdNssr <SdNssr@users.noreply.github.com>
2017-01-25 21:31:06 +0530

    Forgot to remove some old code :)

--- main.go
+++ main.go
@@ -16,17 +16,5 @@ func main() {
 
 	defer db.Close()
 
-	if err := db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists([]byte("timeseries"))
-
-		if err != nil {
-			return fmt.Errorf("create bucket: %s", err)
-		}
-
-		return nil
-	}); err != nil {
-		log.Fatal(err)
-	}
-
 	ServeAPI(ServerEnv{ db: db })
 }
